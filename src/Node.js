04f4e306be8423f774fd06766b84ced41bd1d205
Permutator <permutatorem@gmail.com>
2015-11-26 15:05:08 -0800

    Made creating nodes and upgrading them to emitters separate

--- src/Node.js
+++ src/Node.js
@@ -8,51 +8,26 @@ function pushB(a, b) {
   this.push(b);
 }
 
-export function create(emit) {
+export function create() {
   let self;
   const nodeMap = new Map();
-  let adders, updaters, removers;
-  if(emit) {
-    adders = new Set(); updaters = new Set(); removers = new Set();
-  }
   
-  const set = emit ?
-    (node, value) => {
-      const haveIt = nodeMap.has(node);
-      if(!haveIt || nodeMap.get(node) !== value) {
-        nodeMap.set(node, value);
-        node.set(self, value);
-        (haveIt ? updaters : adders).forEach(evaluate2, [node, value]);
-        return true;
-      }
-      return false;
-    } :
-    (node, value) => {
-      if((value === undefined && !nodeMap.has(node)) || nodeMap.get(node) !== value) {
-        nodeMap.set(node, value);
-        node.set(self, value);
-        return true;
-      }
-      return false;
-    };
-  const remove = emit ?
-    node => {
-      if(nodeMap.has(node)) {
-        nodeMap.delete(node);
-        node.remove(self);
-        removers.forEach(evaluate1, node);
-        return true;
-      }
-      return false;
-    } :
-    node => {
-      if(nodeMap.has(node)) {
-        nodeMap.delete(node);
-        node.remove(self);
-        return true;
-      }
-      return false;
-    };
+  const set = (node, value) => {
+    if((value === undefined && !nodeMap.has(node)) || nodeMap.get(node) !== value) {
+      nodeMap.set(node, value);
+      node.set(self, value);
+      return true;
+    }
+    return false;
+  };
+  const remove = node => {
+    if(nodeMap.has(node)) {
+      nodeMap.delete(node);
+      node.remove(self);
+      return true;
+    }
+    return false;
+  };
   const removeB = (a, b) => remove(b);
   const clear = () => {
     nodeMap.forEach(removeB);
@@ -64,40 +39,61 @@ export function create(emit) {
     nodeMap.forEach(pushB, keys);
     return keys;
   };
-  if(emit) {
-    const onAdd = adder => {
-      adders.add(adder);
-    };
-    const onUpdate = updater => {
-      updaters.add(updater);
-    };
-    const onSet = setter => {
-      adders.add(setter);
-      updaters.add(setter);
-    };
-    const onRemove = remover => {
-      removers.add(remover);
-    };
-    const offAdd = adder => adder ? adders.delete(adder) : adders.clear();
-    const offUpdate = updater => updater ? updaters.delete(updater) : updaters.clear();
-    const offSet = setter => {
-      if(setter) {
-        const adder = adders.delete(setter);
-        return updaters.delete(setter) || adder;
-      }
-      adders.clear(); updaters.clear();
-    };
-    const offRemove = remover => remover ? removers.delete(remover) : removers.clear();
-    const off = () => {
-      adders.clear(); updaters.clear(); removers.clear();
-    }
-    return self = {
-      set, remove, clear, has, get, nodes,
-      onAdd, onUpdate, onSet, onRemove,
-      offAdd, offUpdate, offSet, offRemove, off
-    };
-  }
   return self = {
     set, remove, clear, has, get, nodes
   };
+}
+
+export function makeEmitter(node) {
+  const adders = new Set(), updaters = new Set(), removers = new Set();
+  
+  const set = node.set;
+  node.set = (node, value) => {
+    const haveIt = nodeMap.has(node);
+    if(set(node, value)) {
+      (haveIt ? updaters : adders).forEach(evaluate2, [node, value]);
+      return true;
+    }
+    return false;
+  };
+  const remove = node.remove;
+  node.remove = node => {
+    if(remove(node)) {
+      removers.forEach(evaluate1, node);
+      return true;
+    }
+    return false;
+  };
+  
+  node.onAdd = adder => {
+    adders.add(adder);
+  };
+  node.onUpdate = updater => {
+    updaters.add(updater);
+  };
+  node.onSet = setter => {
+    adders.add(setter);
+    updaters.add(setter);
+  };
+  node.onRemove = remover => {
+    removers.add(remover);
+  };
+  node.offAdd = adder => adder ? adders.delete(adder) : adders.clear();
+  node.offUpdate = updater => updater ? updaters.delete(updater) : updaters.clear();
+  node.offSet = setter => {
+    if(setter) {
+      const adder = adders.delete(setter);
+      return updaters.delete(setter) || adder;
+    }
+    adders.clear(); updaters.clear();
+  };
+  node.offRemove = remover => remover ? removers.delete(remover) : removers.clear();
+  node.off = () => {
+    adders.clear(); updaters.clear(); removers.clear();
+  }
+  return node;
+}
+
+export function createEmitter() {
+  return makeEmitter(create());
 }
