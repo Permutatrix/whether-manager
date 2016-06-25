d8d533254ef350103840620e3ee71b7fbc9fad1b
Permutator <permutatorem@gmail.com>
2016-06-25 14:37:21 -0700

    Added code coverage

---
+++ README.md
@@ -0,0 +1,7 @@
+# whether-manager [![Build Status][travis-image]][travis-url] [![Code Coverage][codecov-image]][codecov-url]
+
+
+[codecov-url]: https://codecov.io/gh/Permutatrix/whether-manager
+[codecov-image]: https://img.shields.io/codecov/c/github/Permutatrix/whether-manager/master.svg
+[travis-url]: https://travis-ci.org/Permutatrix/whether-manager
+[travis-image]: https://img.shields.io/travis/Permutatrix/whether-manager/master.svg
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var babel = require('gulp-babel');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
+var istanbul = require('istanbul');
+var Mocha = require('mocha');
+var MochaSpecReporter = require('mocha/lib/reporters/spec');
+var path = require('path');
 
 gulp.task('default', function() {
   return rollup({ entry: './src/whether-manager.js' })
@@ -12,4 +16,34 @@ gulp.task('default', function() {
   .pipe(buffer())
   .pipe(babel({ presets: ['es2015-minimal'] }))
   .pipe(gulp.dest('.'));
+});
+
+gulp.task('instrumented', function() {
+  return rollup({
+    entry: './src/whether-manager.js',
+    plugins: [require('rollup-plugin-istanbul')()]
+  })
+  .pipe(source('whether-manager.js', './src'))
+  .pipe(buffer())
+  .pipe(babel({ presets: ['es2015-minimal'] }))
+  .pipe(gulp.dest('.'));
+});
+
+gulp.task('test', ['instrumented'], function(cb) {
+  var mocha = new Mocha({
+    reporter: function(runner) {
+      var collector = new istanbul.Collector();
+      var reporter = new istanbul.Reporter();
+      reporter.addAll(['lcov', 'html']);
+      new MochaSpecReporter(runner);
+      
+      runner.on('end', function() {
+        collector.add(global.__coverage__);
+        
+        reporter.write(collector, true, cb);
+      });
+    }
+  });
+  mocha.addFile(path.resolve(process.cwd(), 'test/test.js'));
+  mocha.run();
 });
