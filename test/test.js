e49732c0e893178497b5ce5dc44ba813a53b4a3e
Permutator <permutatorem@gmail.com>
2016-06-26 13:40:39 -0700

    Reused Nodes tests for Supernodes

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 var demand = require('must');
 var wm = require('..');
 
-describe("Nodes", function() {
+function describeNodes(node) {
+  
+  it("should be frozen", function() {
+    var a = node();
+    demand(Object.isFrozen(a)).be.truthy();
+  });
   
   it("should be linked symmetrically", function() {
-    var a = wm.node(), b = wm.node();
+    var a = node(), b = node();
     a.set(b, 'x');
     demand(a.has(b)).be.true();
     demand(b.has(a)).be.true();
@@ -13,7 +18,7 @@ describe("Nodes", function() {
   });
   
   it("should be unlinked symmetrically", function() {
-    var a = wm.node(), b = wm.node();
+    var a = node(), b = node();
     a.set(b, 'x');
     b.remove(a);
     demand(a.has(b)).be.false();
@@ -23,19 +28,19 @@ describe("Nodes", function() {
   describe(".set()", function() {
     
     it("should throw when called with a non-node", function() {
-      var a = wm.node();
+      var a = node();
       demand(function() { a.set({}); }).to.throw(/non-node/i);
     });
     
     it("should return true if there's a change", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.set(b)).be.true();
       demand(a.set(b, 'x')).be.true();
       demand(a.set(b)).be.true();
     });
     
     it("should return false otherwise", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       a.set(b);
       demand(a.set(b)).be.false();
       a.set(b, 'x');
@@ -47,7 +52,7 @@ describe("Nodes", function() {
   describe(".remove()", function() {
     
     it("should return true if there's a change", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       a.set(b);
       demand(a.remove(b)).be.true();
       a.set(b, 'x');
@@ -55,7 +60,7 @@ describe("Nodes", function() {
     });
     
     it("should return false otherwise", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.remove(b)).be.false();
       a.set(b, 'x');
       a.remove(b);
@@ -67,13 +72,13 @@ describe("Nodes", function() {
   describe(".has()", function() {
     
     it("should return true if the nodes are linked", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       a.set(b);
       demand(a.has(b)).be.true();
     });
     
     it("should return false otherwise", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.has(b)).be.false();
       a.set(b);
       a.remove(b);
@@ -85,13 +90,13 @@ describe("Nodes", function() {
   describe(".get()", function() {
     
     it("should return the nodes' link value", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       a.set(b, 'x');
       demand(a.get(b)).equal('x');
     });
     
     it("should return undefined if the nodes aren't linked", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.get(b)).be.undefined();
       a.set(b, 'x');
       a.remove(b);
@@ -99,7 +104,7 @@ describe("Nodes", function() {
     });
     
     it("should return undefined if the nodes' link value is undefined", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       a.set(b);
       demand(a.get(b)).be.undefined();
     });
@@ -109,7 +114,7 @@ describe("Nodes", function() {
   describe(".getNodes()", function() {
     
     it("should return an array of linked nodes", function() {
-      var a = wm.node(), b = wm.node(), c = wm.node();
+      var a = node(), b = node(), c = node();
       demand(a.getNodes()).be.empty();
       a.set(b);
       demand(a.getNodes()).eql([b]);
@@ -124,14 +129,14 @@ describe("Nodes", function() {
     });
     
     it("should return a new array on each invocation by default", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.getNodes()).not.equal(a.getNodes());
       a.set(b);
       demand(a.getNodes()).not.equal(a.getNodes());
     });
     
     it("should return a new array on each invocation when passed a truthy value", function() {
-      var a = wm.node(), b = wm.node();
+      var a = node(), b = node();
       demand(a.getNodes(false)).not.equal(a.getNodes(1));
       a.set(b);
       demand(a.getNodes(false)).not.equal(a.getNodes(true));
@@ -144,13 +149,13 @@ describe("Nodes", function() {
   describe(".name", function() {
     
     it("should be equal to the passed-in name", function() {
-      var a = wm.node('a'), b = wm.node('b');
+      var a = node('a'), b = node('b');
       demand(a.name).equal('a');
       demand(b.name).equal('b');
     });
     
     it("should be undefined if no name is given", function() {
-      var a = wm.node();
+      var a = node();
       demand(a.name).be.undefined();
     });
     
@@ -158,6 +163,33 @@ describe("Nodes", function() {
   
   describe(".nodes", function() {
     
+    it("should be an array of linked nodes", function() {
+      var a = node(), b = node(), c = node();
+      demand(a.nodes).be.empty();
+      a.set(b);
+      demand(a.nodes).eql([b]);
+      a.set(c);
+      demand(a.nodes).have.length(2);
+      demand(a.nodes).include(b);
+      demand(a.nodes).include(c);
+      a.remove(b);
+      demand(a.nodes).eql([c]);
+      a.remove(c);
+      demand(a.nodes).be.empty();
+    });
+    
   });
+  
+}
+
+describe("Nodes", function() {
+  
+  describeNodes(wm.node);
+  
+});
+
+describe("Supernodes", function() {
+  
+  describeNodes(wm.supernode);
   
 });
